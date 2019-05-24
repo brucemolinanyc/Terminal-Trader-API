309e0b3d847b0ce305ee25e88adc9f3fe1205fb2
brucemolinanyc <brucemolinanyc@gmail.com>
2019-05-24 12:09:31 -0400

    routes test

--- backend/flask_app/routes.py
+++ backend/flask_app/routes.py
@@ -20,9 +20,9 @@ def error404():
 def error500():
     return jsonify({"error": "application error"}), 500
 
-# @app.route("/")
-# def helloWorld():
-#     return jsonify({"test": "hello world"}) 
+@app.route("/")
+def helloWorld():
+    return jsonify({"test": "hello world"}) 
 
 @app.route('/user/<id>')
 def user(id):
